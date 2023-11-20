2171f39d576f68f274fd79d7c79be3ad28e47b7b
zizhou-Dell <zeowang@ece.ubc.ca>
2023-11-19 18:47:51 -0800

    Add hosting event on the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,32 @@ import React from 'react';
 import './App.css';
 import textbox from './components/textbox';
 import logo from './logo.svg';
+import nw from './nwPlus_Logo.svg';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
+                <img src={nw} className="topRightImage" alt="nw" />
             </header>
             <section className="App-center">
                 Welcome to
                 <code>HateHalt</code>a ml-based hatred semantic detector.
                 <a>{textbox()}</a>
             </section>
-            <section className="App-footer"></section>
+            <section className="App-footer">
+                <p>
+                    <a
+                        className="github-link"
+                        href="https://github.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        GitHub
+                    </a>
+                </p>
+            </section>
         </div>
     );
 }
