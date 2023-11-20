81b3a96d930332b5aec08e49cb164a4cad0230b4
zizhou-Dell <zeowang@ece.ubc.ca>
2023-11-19 16:48:35 -0800

    Add default styling to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
 import textbox from './components/textbox';
+import logo from './logo.svg';
 
 function App() {
-    return <div className="App">{textbox()}</div>;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Welcome to <code>HateHalt</code>, a react-based hatred semantic detector.
+                </p>
+                <a>
+                    {textbox()}
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
