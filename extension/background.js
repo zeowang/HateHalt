64b7ac32c6ce3336eb3063fd8bce23e329cde43a
dayshaunlee <leedayshaun@gmail.com>
2023-11-19 19:19:30 -0800

    added more changes to extension files

--- extension/background.js
+++ extension/background.js
@@ -8,57 +8,89 @@
 // Add a listener to create the initial context menu items,
 // context menu items only need to be created at runtime.onInstalled
 chrome.runtime.onInstalled.addListener(async () => {
-  chrome.contextMenus.create({
-    id: "selection",
-    title: "Scan hate speech",
-    type: "normal",
-    contexts: ["selection"],
+    chrome.contextMenus.create({
+      id: "selection",
+      title: "Scan hate speech",
+      type: "normal",
+      contexts: ["selection"],
+    });
   });
-});
-
-const texts = new Set();
-
-export function getTexts() {
-  return texts;
-}
-
-chrome.contextMenus.onClicked.addListener(async (item) => {
-  console.log("Clicked context menu");
-  // get text
-  let text = item.selectionText;
-  // do something with the text
-  console.log('Selected text: "' + text + '"');
-  texts.add(text);
-
-  fetch_label(text);
-  // Save the updated set to chrome.storage.sync
-  chrome.storage.sync.set({ texts: Array.from(texts) }, () => {
-    console.log("Texts saved to storage:", Array.from(texts));
-  });
-});
-
-// function that query backend given the string
-const fetch_label = async (string) => {
-  const json_data = {
-    strings: [string],
-  };
-
-  // post request
-  const response = await fetch("http://localhost:5000/api/detect", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(json_data),
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
+  
+  const textsMap = new Map();
+  const texts = new Set();
+  
+  export function getTexts() {
+    return textsMap;
   }
-
-  const data = await response.json();
-  // Do something with the response data
-  console.log("Response:", data);
-
-  return data;
-};
+  
+  chrome.contextMenus.onClicked.addListener(async (item) => {
+    console.log("Clicked context menu");
+    // get text
+    let text = item.selectionText;
+    // do something with the text
+    console.log('Selected text: "' + text + '"');
+    texts.add(text);
+  
+    fetch_label(text);
+  });
+  
+  // function that query backend given the string
+  const fetch_label = async (string) => {
+    const json_data = {
+      strings: [string],
+    };
+  
+    // post request
+    const response = await fetch("http://localhost:5000/api/detect", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(json_data),
+    });
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+  
+    const data = await response.json();
+    // Do something with the response data
+    console.log("Response:", data);
+    
+    textsMap.set(string, data);
+    // Save the updated set to chrome.storage.sync
+    chrome.storage.sync.set(textsMap, () => {
+        console.log("Texts saved to storage:", textsMap);
+      });
+    return data;
+  };
+  
+//   document.getElementById("openReactPage").addEventListener("click", function () {
+//     const indexHtmlURL = chrome.runtime.getURL("../frontend/build/index.html");
+  
+//     // Get the base URL (directory) of the index.html file
+//     const baseUrl = indexHtmlURL.substring(
+//       0,
+//       indexHtmlURL.lastIndexOf("/frontend/build") + 1
+//     );
+  
+//     // Specify the relative paths of your static files
+//     const staticFiles = [
+//       "static/css/main.css",
+//       "static/js/main.27ebd520.js",
+//       // Add more static files as needed
+//     ];
+  
+//     // Create an array of URLs for the static files
+//     const staticFileURLs = staticFiles.map((file) => `${baseUrl}${file}`);
+  
+//     // Open a new tab with the index.html and static files
+//     chrome.tabs.create({ url: indexHtmlURL });
+  
+//     // staticFiles.forEach((file) => {
+//     //   const staticFileURL = `${baseUrl}${file}`;
+//     //   chrome.tabs.create({ url: staticFileURL });
+//     // });
+  
+  
+//   });
--- extension/popup.js
+++ extension/popup.js
@@ -1,21 +1,58 @@
 // popup.js
-import { getTexts } from './background.js';
 
 createForm().catch(console.error);
 
+import { getTexts } from './background.js';
+
 async function createForm() {
     // add text to data storage
-    const texts = getTexts();
-    const storedTexts = await chrome.storage.sync.get(['texts']);
+    const textsMap = getTexts();
+    const storedTextsMap = await chrome.storage.sync.get(['textsMap']);
+    console.log("test", storedTextsMap);
+    const storedTexts = storedTextsMap.textsMap();
 
     if (storedTexts.texts) {
         const textsArray = Array.from(storedTexts.texts);
+
         let parsedTexts = "";
         for (let i = 0, len = textsArray.length; i < len; i++) {
             parsedTexts += textsArray[i] + "\n";
         }
         console.log("Stored texts:\n", parsedTexts);
         console.log(textsArray);
+
+        // add text to list in html
+        let list = document.getElementById("texts");
+        for (let i = 0; i < textsArray.length; ++i) {
+            let li = document.createElement('li');
+
+            // text label
+            let label = document.createElement('span')
+            label.innerText = textsArray[i]
+            li.appendChild(label);
+
+            // color code
+            let span = document.createElement('span')
+            if (textsMap.get(textsArray[i]).labels[0] == "hate_speech") {
+                span.classList.add("list-key-hate");
+            }
+            if (textsMap.get(textsArray[i]).labels[0] == "offensive_language") {
+                span.classList.add("list-key-offensive");
+            }
+            if (textsMap.get(textsArray[i]).labels[0] == "neither") {
+                span.classList.add("list-key-neither");
+            }
+            li.appendChild(span);
+
+            // probability number
+            let probability = document.createElement('span');
+            let percentage = Math.round(Math.max(...textsMap.get(textsArray[i]).prob) * 100);
+            probability.innerText = percentage.toString + "%";
+            li.appendChild(probability);
+
+            list.appendChild(li);
+            console.log("Added " + li.innerText + " to HTML")
+        }
     } else {
         console.log("No stored texts");
     }
